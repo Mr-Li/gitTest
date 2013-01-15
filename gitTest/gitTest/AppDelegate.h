5831d1d9550f8acdeb2e37a75bc1808309eaa57e
Mr.Li <codingwithme@163.com>
2013-01-15 15:19:40 +0800

    test

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Li Dongke on 13-1-15.
+//  Copyright (c) 2013年 Li Dongke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitTest/gitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by Li Dongke on 13-1-15.
+//  Copyright (c) 2013年 Li Dongke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
